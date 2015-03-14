b0c69d6dec7fd21557b143a371815fa35d4dcf26
Paul Scherer <schererpaul@rocketmail.com>
2015-03-14 23:54:56 +0000

    Pictures Again

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ def hello_monkey():
 	"""Respond to incoming calls with a simple text message."""
 	resp = twilio.twiml.Response()
 	with resp.message("Hello, Mobile Monkey") as m:
-	m.media("http://i.imgur.com/O2NQNvP.jpg")
+		m.media("http://i.imgur.com/O2NQNvP.jpg")
 	return str(resp)
 
 if __name__ == "__main__":
